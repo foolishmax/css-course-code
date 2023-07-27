e50c9592f8629727d319b450b3ef6fcb49b622c4
foolishmax <827864735@qq.com>
2023-07-27 14:32:15 +0800

    feat(init): init

---
+++ src/css最强大的background的8中核心技巧/index.tsx
@@ -0,0 +1,16 @@
+import './index.less'
+
+export default function App() {
+  return (
+    <div className="g-container">
+      <ul>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+        <li>Button</li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './css最强大的background的8中核心技巧'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
